0f1ff79a8412e3d6a347d692d318a6b3cb6945e7
fajarfadillahh <fajaragustian979@gmail.com>
2023-01-09 23:04:36 +0700

    create Share.jsx, the part of Homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import Header from "../components/Header";
 import Hero from "../parts/Homepage/Hero";
+import Share from "../parts/Homepage/Share";
 
 export const Homepage = () => {
   return (
     <>
       <Header />
       <Hero />
+      <Share />
     </>
   );
 };
---
+++ src/parts/Homepage/Share.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import ShareImg from "../../assets/images/shared.png";
+
+export default function Share() {
+  return (
+    <div className="share section">
+      <div className="share__container container grid gap-12">
+        <div className="share__data justify-self-center text-center">
+          <h1 className="share__title section-title">
+            Sharing Is The Best Gift <br />
+            Christmas
+          </h1>
+          <p className="share__text section-text">
+            Sharing these holidays is the best gift you can give, give a present
+            or share your love with the people you love the most and celebrate
+            with great happiness.
+          </p>
+          <Link to="/" className="share__button button inline-flex">
+            Send a gift
+          </Link>
+        </div>
+
+        <img
+          src={ShareImg}
+          className="share__img w-72 justify-self-center"
+          alt="Share image"
+        />
+      </div>
+    </div>
+  );
+}
