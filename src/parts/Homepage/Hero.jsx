e8a62546b0e5a2f954387679c42083437aecbdc8
fajarfadillahh <fajaragustian979@gmail.com>
2023-01-10 09:07:45 +0700

    responsive Hero.jsx & modified _reusables.css file

--- src/parts/Homepage/Hero.jsx
+++ src/parts/Homepage/Hero.jsx
@@ -6,18 +6,18 @@ import HeroImg from "../../assets/images/home.png";
 export default function Hero() {
   return (
     <div className="hero section">
-      <div className="hero__container container grid gap-12">
+      <div className="hero__container container grid gap-12 sm:grid-cols-2 sm:items-center lg:max-w-4xl">
         <img
           src={HeroImg}
-          className="hero__img w-72 justify-self-center"
+          className="hero__img w-72 justify-self-center sm:order-2 md:w-auto"
           alt="Hero image"
         />
 
         <div className="hero__data justify-self-center">
-          <h1 className="hero__title section-title text-4xl">
+          <h1 className="hero__title section-title text-[36px] md:text-[52px]">
             Send A Gift For Christmas
           </h1>
-          <p className="hero__text section-text">
+          <p className="hero__text section-text md:w-[320px]">
             In these of the year holidays send a gift to your loved one and
             share the happiness at Christmas.
           </p>
