97a97182e52e7b1e0974b8b344ba888c23cac277
fajarfadillahh <fajaragustian979@gmail.com>
2023-01-10 10:31:57 +0700

    responsive Send.jsx

--- src/components/Send.jsx
+++ src/components/Send.jsx
@@ -5,9 +5,9 @@ import SendImg from "../assets/images/send.png";
 export default function Send() {
   return (
     <div className="send section">
-      <div className="send__bg bg-red-600 py-12">
-        <div className="send__container container grid gap-12">
-          <div className="send__data justify-self-center text-center">
+      <div className="send__bg bg-red-600 py-12 md:bg-transparent md:py-0">
+        <div className="send__container container grid gap-12 sm:grid-cols-2 sm:items-center md:rounded-3xl md:bg-red-600 md:py-24 md:px-16 lg:max-w-4xl lg:grid-cols-[.8fr,1fr]">
+          <div className="send__data justify-self-center text-center sm:text-left">
             <h1 className="send__title section-title text-white">
               Send Gift Now
             </h1>
@@ -21,7 +21,7 @@ export default function Send() {
             >
               <input
                 type="email"
-                className="send__input h-[52px] pl-2 text-[15px] font-semibold text-gray-900 outline-none placeholder:font-normal placeholder:text-gray-600"
+                className="send__input h-[52px] w-full pl-2 text-[15px] font-semibold text-gray-900 outline-none placeholder:font-normal placeholder:text-gray-600"
                 placeholder="Email address"
               />
               <button className="send__button button">Send</button>
